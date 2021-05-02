0139bfe6d26653d4fcb89f5452f22b6462714561
byunahakjang1230 <byunghakjang1230@gmail.com>
2021-05-03 01:08:35 +0900

    feat(Car.java): 입력받은 숫자에 따라 자동차가 이동하거나 멈추는 기능 추가
    test(CarTest.java): 자동차 이동, 멈춤을 검증하는 테스트 코드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,26 @@
 package racingcar.domain;
 
 public class Car {
+	public static final int MOVABLE_MIN_NUMBER = 4;
 	private final CarName carName;
+	private int moveCount;
 
 	public Car(String carName) {
 		this.carName = new CarName(carName);
+		this.moveCount = 0;
 	}
 
 	public String getCarName() {
 		return this.carName.getCarName();
+	}
+
+	public void tryMove(int randomNo) {
+		if (randomNo >= MOVABLE_MIN_NUMBER) {
+			this.moveCount++;
+		}
+	}
+
+	public int getMoveCount() {
+		return this.moveCount;
 	}
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -8,6 +8,16 @@ import org.junit.jupiter.api.Test;
 public class CarTest {
 
 	@Test
+	@DisplayName("자동차_이동_테스트")
+	public void car_move_test() {
+		Car car = new Car("abc");
+		car.tryMove(4);
+		assertThat(car.getMoveCount()).isEqualTo(1);
+		car.tryMove(3);
+		assertThat(car.getMoveCount()).isEqualTo(1);
+	}
+
+	@Test
 	@DisplayName("자동차이름_부여_테스트")
 	public void car_name_set_test() {
 		Car car1 = new Car("a");
