73127c21a1d443bb355bf739073f773fbf07dc1f
byunahakjang1230 <byunghakjang1230@gmail.com>
2021-05-03 23:50:38 +0900

    feat(Race.java): 경기 중 총 이동 횟수를 저장하고 입력값을 검증하는 기능 추가
    feat(RaceTest.java): 이동 횟수 저장 및 예외처리 검증 테스트 코드 추가

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -8,10 +8,13 @@ import java.util.Set;
 
 public class Race {
 	public static final String SPLIT_REGEX = ",";
+	public static final int MIN_RACING_COUNT = 1;
 	private final List<Car> carList;
+	private int racingCount;
 
 	public Race() {
 		this.carList = new ArrayList<>();
+		this.racingCount = 0;
 	}
 
 	public void createCars(String carNames) {
@@ -26,11 +29,26 @@ public class Race {
 		return this.carList.size();
 	}
 
+	public void setRacingCount(int racingCount) {
+		validateRacingCount(racingCount);
+		this.racingCount = racingCount;
+	}
+
+	public int getRacingCount() {
+		return this.racingCount;
+	}
+
 	private void validateDuplicationCarName(String[] carNames) {
 		Set<String> validSet = new HashSet<>();
 		validSet.addAll(Arrays.asList(carNames));
 		if (validSet.size() != carNames.length) {
 			throw new IllegalArgumentException("중복된 자동차 이름이 입력되었습니다.");
+		}
+	}
+
+	private void validateRacingCount(int racingCount) {
+		if (racingCount < MIN_RACING_COUNT) {
+			throw new IllegalArgumentException("이동 횟수는 1 이상의 양의 정수만 가능합니다.");
 		}
 	}
 }
--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -8,6 +8,17 @@ import org.junit.jupiter.api.Test;
 public class RaceTest {
 
 	@Test
+	@DisplayName("이동_횟수_입력_테스트")
+	public void racing_count_set_test() {
+		Race race = new Race();
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			race.setRacingCount(0);
+		});
+		race.setRacingCount(1);
+		assertThat(race.getRacingCount()).isEqualTo(1);
+	}
+
+	@Test
 	@DisplayName("자동차_리스트_생성")
 	public void create_cars_test() {
 		Race race1 = new Race();
